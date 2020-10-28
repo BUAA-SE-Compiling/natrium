2fc7e166bec1a506004c5eddbb8cddc081dabdce
Rynco Maekawa <lynzrand@outlook.com>
2020-10-28 13:50:20 +0800

    feat: compile stuff

--- crates/codegen-r0vm/src/lib.rs
+++ crates/codegen-r0vm/src/lib.rs
@@ -2,16 +2,21 @@ pub mod err;
 pub mod scope;
 pub mod ty;
 
+use std::cell::RefCell;
+
 use err::{CompileError, CompileErrorKind};
 use r0syntax::{ast, util::Mut, util::MutWeak};
 use r0vm::s0;
-use scope::{Scope, Symbol};
+use scope::{Scope, Symbol, SymbolIdGenerator};
 use ty::Ty;
+
+static RET_VAL_KEY: &str = "$ret";
 
 type Result<T> = std::result::Result<T, CompileError>;
 
 pub fn compile(tree: &ast::Program) -> Result<s0::S0> {
-    let mut global_scope = Scope::new();
+    let global_sym_gen = RefCell::new(SymbolIdGenerator::new());
+    let mut global_scope = Scope::new(&global_sym_gen);
     for decl in &tree.decls {
         compile_decl(decl, &mut global_scope)?;
     }
@@ -22,20 +27,37 @@ pub fn compile(tree: &ast::Program) -> Result<s0::S0> {
 }
 
 fn compile_func(func: &ast::FuncStmt, global_scope: &Scope) -> Result<s0::FnDef> {
-    let mut func_scope = Scope::new_with_parent(global_scope);
+    let mut scope = Scope::new_with_parent(global_scope);
+
+    scope.insert(
+        RET_VAL_KEY.into(),
+        Symbol::new(get_ty(&func.ret_ty)?, false),
+    );
+
+    for param in &func.params {
+        scope.insert(
+            param.name.val.clone(),
+            Symbol::new(get_ty(&param.ty)?, false),
+        );
+    }
+
+    compile_block(&func.body, &scope)?;
+
     todo!()
+}
+
+fn compile_block(blk: &ast::BlockStmt, scope: &Scope) -> Result<()> {
+    Ok(())
 }
 
 fn compile_decl(decl: &ast::DeclStmt, scope: &mut Scope) -> Result<()> {
     let ty = get_ty(&decl.ty)?;
     let name = decl.name.val.clone();
 
-    let symbol = Symbol {
-        ty,
-        is_const: decl.is_const,
-    };
+    let symbol = Symbol::new(ty, decl.is_const);
 
-    if !scope.insert(name, symbol) {
+    let symbol = scope.insert(name, symbol);
+    if symbol.is_none() {
         return Err(CompileError {
             kind: CompileErrorKind::DuplicateSymbol(decl.name.val.as_str().into()),
             span: Some(decl.name.span),
--- crates/codegen-r0vm/src/scope.rs
+++ crates/codegen-r0vm/src/scope.rs
@@ -1,10 +1,30 @@
+use std::{cell::RefCell, sync::atomic::AtomicU64};
+
 use indexmap::IndexMap;
 use smol_str::SmolStr;
 
 use crate::ty::Ty;
 
 #[derive(Debug)]
+pub struct SymbolIdGenerator {
+    next_id: u64,
+}
+
+impl SymbolIdGenerator {
+    pub fn new() -> SymbolIdGenerator {
+        SymbolIdGenerator { next_id: 0 }
+    }
+
+    pub fn next(&mut self) -> u64 {
+        let id = self.next_id;
+        self.next_id += 1;
+        id
+    }
+}
+
+#[derive(Debug)]
 pub struct Scope<'p> {
+    symbol_gen: &'p RefCell<SymbolIdGenerator>,
     pub parent: Option<&'p Scope<'p>>,
     pub vars: IndexMap<SmolStr, Symbol>,
 }
@@ -13,13 +33,15 @@ pub struct Scope<'p> {
 impl<'p> Scope<'p> {
     pub fn new_with_parent(parent: &'p Scope<'p>) -> Scope<'p> {
         Scope {
+            symbol_gen: parent.symbol_gen,
             parent: Some(parent),
             vars: IndexMap::new(),
         }
     }
 
-    pub fn new() -> Scope<'p> {
+    pub fn new(symbol_gen: &'p RefCell<SymbolIdGenerator>) -> Scope<'p> {
         Scope {
+            symbol_gen,
             parent: None,
             vars: IndexMap::new(),
         }
@@ -40,13 +62,15 @@ impl<'p> Scope<'p> {
         self_res
     }
 
-    pub fn insert(&mut self, ident: SmolStr, symbol: Symbol) -> bool {
+    pub fn insert(&mut self, ident: SmolStr, mut symbol: Symbol) -> Option<u64> {
         let entry = self.vars.entry(ident);
         match entry {
-            indexmap::map::Entry::Occupied(_) => false,
+            indexmap::map::Entry::Occupied(_) => None,
             indexmap::map::Entry::Vacant(v) => {
+                let id = self.symbol_gen.borrow_mut().next();
+                symbol.id = id;
                 v.insert(symbol);
-                true
+                Some(id)
             }
         }
     }
@@ -54,6 +78,17 @@ impl<'p> Scope<'p> {
 
 #[derive(Debug, Clone)]
 pub struct Symbol {
+    pub id: u64,
     pub ty: Ty,
     pub is_const: bool,
+}
+
+impl Symbol {
+    pub fn new(ty: Ty, is_const: bool) -> Symbol {
+        Symbol {
+            ty,
+            is_const,
+            id: 0,
+        }
+    }
 }
